f3433c1c5b7a745d3121dfb7c7818bffb7bfb040
witoldkesek <witoldkesek@gmail.com>
2022-03-11 15:42:51 +0100

    refactor: vendor category root mocked, what removes second api call

--- packages/api-client/src/api/getCategory/index.ts
+++ packages/api-client/src/api/getCategory/index.ts
@@ -1,28 +1,38 @@
 import { ApiContext, Category, CategorySearchResult, GetCategoryParams } from '../../types';
 import { deserializeCategories } from '../serializers/category';
+import {TaxonAttr} from '@spree/storefront-api-v2-sdk/types/interfaces/Taxon';
 
 const findCategory = (categories: Category[], slug: string) => categories.find(e => e.slug === slug);
 
+const mockRoot = (data) => {
+  return {
+    relationships: {
+      children: {data: data.map(e => {
+        return {id: e.id, type: 'taxon'};
+      })},
+      parent: null
+    },
+    attributes: {
+      name: 'Categories',
+      permalink: 'categories',
+      is_root: true
+    },
+    id: 'categories_id',
+    type: 'taxon'
+  };
+};
 export default async function getCategory({ client }: ApiContext, { categorySlug, vendorId }: GetCategoryParams): Promise<CategorySearchResult> {
   const result = await client.taxons.list({filter: {vendor_id: vendorId}, fields: { taxon: 'name,permalink,children,parent,is_root' }, per_page: 500 });
-  const rootResult = await client.taxons.show('categories', { fields: { taxon: 'name,permalink,children,parent,is_root' }, per_page: 500 });
+
   if (result.isSuccess()) {
     try {
       const data = result.success().data;
-      const rootResultData = rootResult.success().data;
       if (vendorId) {
-        data.forEach((element,index) => {
-          element.relationships.parent.data['id'] = rootResultData.id;
-          let children = rootResultData.relationships.children.data;
-          if (Array.isArray(children)) {
-            children.push({id: element.id, type: 'taxon'});
-          } else {
-            children = [children, {id: element.id, type: 'taxon'}];
-          }
-          rootResultData.relationships.children.data = children;
-
+        const root = mockRoot(data);
+        data.forEach(e => {
+          e.relationships.parent.data['id'] = root.id;
         });
-        data.push(rootResultData);
+        data.push(<TaxonAttr><unknown>root);
       }
       const categories = deserializeCategories(data);
       return {
